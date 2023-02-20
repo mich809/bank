dfc637eb4e13cab1f6508716928a9ab1d5bb2c74
Michael Caridad <caridadmichael@gmail.com>
2023-02-20 16:36:49 -0400

    add OneToOne relationship to account , and update constructor and getter/set

--- src/main/java/com/caridadmichael/bank/model/Customer.java
+++ src/main/java/com/caridadmichael/bank/model/Customer.java
@@ -4,37 +4,61 @@ import java.util.Date;
 
 import org.springframework.data.annotation.CreatedDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+
 
 @Entity
 public class Customer {
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
 	private Long id;
+
 	private String firstName;
 	private String middleName;
 	private String lastName;
+
+	private String password;
 	private String city;
 	private String telephone;
 	private String occupation;
 	@CreatedDate
 	private Date creationDate;
 
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "account_id", referencedColumnName = "id")
+	private Account account;
+
 	public Customer() {
 	}
 
-	public Customer(String firstName, String middleName, String lastName, String city, String telephone,
-			String occupation) {
+	public Customer(Long id, String firstName, String middleName, String lastName, String city, String telephone,
+			String occupation, Account account) {
+		this.id = id;
 		this.firstName = firstName;
 		this.middleName = middleName;
 		this.lastName = lastName;
 		this.city = city;
 		this.telephone = telephone;
 		this.occupation = occupation;
+		this.account = account;
 
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return this.id;
 	}
 
 	public String getFirstName() {
@@ -91,6 +115,22 @@ public class Customer {
 
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
 	}
 
 }
