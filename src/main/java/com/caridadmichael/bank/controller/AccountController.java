8f8cbd0f962c25bd6b51852d8f16f3825bde0ffe
Michael Caridad <caridadmichael@gmail.com>
2023-02-20 16:35:58 -0400

    rename url mapping and add constructor

--- src/main/java/com/caridadmichael/bank/controller/AccountController.java
+++ src/main/java/com/caridadmichael/bank/controller/AccountController.java
@@ -4,22 +4,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caridadmichael.bank.model.Account;
+import com.caridadmichael.bank.service.AccountService;
 
 @RestController
-@RequestMapping("v1/account")
+@RequestMapping("/api/account")
 public class AccountController {
 	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+	private final AccountService accountService;
+	
+	public AccountController(AccountService accountService) {
+		this.accountService = accountService;
+	}
 
-	@PostMapping
-    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
-		logger.info("Account Created");
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+	
+	
+
 }
