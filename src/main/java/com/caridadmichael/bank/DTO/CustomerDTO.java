5e85e8e7a4a35472956215bdc5919a2fd590aec1
Michael Caridad <caridadmichael@gmail.com>
2023-02-21 16:50:23 -0400

    update to list of account

--- src/main/java/com/caridadmichael/bank/DTO/CustomerDTO.java
+++ src/main/java/com/caridadmichael/bank/DTO/CustomerDTO.java
@@ -1,8 +1,10 @@
 package com.caridadmichael.bank.DTO;
 
+import java.util.List;
+
 import com.caridadmichael.bank.model.Account;
 
 public record CustomerDTO(Long id, String firstName, String middleName, String lastName, String city, String telephone,
-		String occupation , Account account) {
+		String occupation , List<Account> account) {
 
 }
