9ddaa841451877fbf6f27e8fba46decdebb5f032
Michael Caridad <caridadmichael@gmail.com>
2023-02-21 16:50:10 -0400

    remove file

--- src/main/java/com/caridadmichael/bank/DTO/CustomerRegistrationRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.caridadmichael.bank.DTO;
-
-public record CustomerRegistrationRequestDTO(String firstName, String middleName, String lastName, String city,
-		String telephone, String occupation) {
-
-}
