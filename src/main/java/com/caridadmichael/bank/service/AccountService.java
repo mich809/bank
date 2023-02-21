69e149050f1043acf63da25d80588ec58c25e0da
Michael Caridad <caridadmichael@gmail.com>
2023-02-21 16:49:58 -0400

    add method to add account to customer

--- src/main/java/com/caridadmichael/bank/service/AccountService.java
+++ src/main/java/com/caridadmichael/bank/service/AccountService.java
@@ -1,20 +1,33 @@
 package com.caridadmichael.bank.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.caridadmichael.bank.model.Account;
+import com.caridadmichael.bank.model.AccountType;
+import com.caridadmichael.bank.model.Customer;
 import com.caridadmichael.bank.repository.AccountRepo;
+import com.caridadmichael.bank.repository.CustomerRepo;
 
 @Service
 public class AccountService {
 	private final AccountRepo accountRepo;
+	private final CustomerRepo customerRepo;
 	
-	public AccountService(AccountRepo accountRepo) {
+	public AccountService(AccountRepo accountRepo , CustomerRepo customerRepo) {
 		this.accountRepo = accountRepo;
+		this.customerRepo = customerRepo;
 	}
 
-	public void getAccount() {
-		// TODO Auto-generated method stub
-		
+	public void createAccount(Long id,Account account) {
+		if (customerRepo.findById(id).isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found.");
+		}		
+		Customer customer = customerRepo.findById(id).get();
+		customer.getAccount().add(account);
+		customerRepo.save(customer);
+	   
 	}
 
 }
