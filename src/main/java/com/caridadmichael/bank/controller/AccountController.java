e0094bb5fce3e764a97f4c4793f497dc0985be20
Michael Caridad <caridadmichael@gmail.com>
2023-02-28 14:29:49 -0400

    add crossOrigin annotation

--- src/main/java/com/caridadmichael/bank/controller/AccountController.java
+++ src/main/java/com/caridadmichael/bank/controller/AccountController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.caridadmichael.bank.service.AccountService;
 
 @RestController
 @RequestMapping("/api/account")
+@CrossOrigin
 public class AccountController {
 	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
