d22a26f57997e99a5f264b50fc5fab1b6a1c7370
Michael Caridad <caridadmichael@gmail.com>
2023-03-05 23:01:10 -0400

    add annotation

--- src/main/java/com/caridadmichael/bank/BankApplication.java
+++ src/main/java/com/caridadmichael/bank/BankApplication.java
@@ -2,7 +2,11 @@ package com.caridadmichael.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.caridadmichael.bank.config.RsaKeyProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class BankApplication {
 
