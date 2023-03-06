8cd1f091a8d284cb428a52df2cf50ba932f47edf
Michael Caridad <caridadmichael@gmail.com>
2023-03-05 23:01:36 -0400

    add two new dependecies and url

--- src/main/java/com/caridadmichael/bank/controller/CustomerController.java
+++ src/main/java/com/caridadmichael/bank/controller/CustomerController.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +24,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caridadmichael.bank.DTO.CustomerDTO;
+import com.caridadmichael.bank.DTO.LoginRequest;
 import com.caridadmichael.bank.model.Customer;
 import com.caridadmichael.bank.service.CustomerService;
+import com.caridadmichael.bank.service.TokenService;
 
 import jakarta.validation.Valid;
 
@@ -31,17 +37,24 @@ import jakarta.validation.Valid;
 public class CustomerController {
 	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 	private final CustomerService customerService;
+	private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
-	public CustomerController(CustomerService customerService) {
+
+	public CustomerController(CustomerService customerService, TokenService tokenService, AuthenticationManager authenticationManager) {
 		this.customerService = customerService;
+		this.tokenService = tokenService;
+		this.authenticationManager = authenticationManager;
 	}
 
-	@PostMapping()
+	@PostMapping("/createCustomer")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createCustomer(@RequestBody  Customer customer) {
-		logger.info(String.format("Account created for: %s %s ", customer.getFirstName(), customer.getLastName()));
+		logger.info(String.format("Account created for: %s ", customer.getUsername()));
 		customerService.addCustomer(customer);
 	}
+	
+	
 
 	@GetMapping("/{id}")
 	public CustomerDTO getCustomerByID(@PathVariable Long id) {
@@ -72,5 +85,12 @@ public class CustomerController {
 		customerService.deleteCustomer(id);
 
 	}
+	
+	@PostMapping("/token")
+    public String token(@RequestBody LoginRequest loginRequest) throws AuthenticationException  {
+      logger.info("Token requested for user: '{}'",loginRequest.username());      
+      Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+      return tokenService.generateToken(authentication);
+    }
 
 }
